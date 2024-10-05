fabbd90fde7233ab31e3a795b6bc205993b47e4a
Fernandomastre32 <20233l001062@utcv.edu.mx>
2024-10-04 18:16:25 -0600

    feat: Se Agrega una opcion de forma en que se pueda eliminar

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log(frutas[0]); // "Manzana"
 console.log(frutas[1]); // "Banana"
 frutas.push("Mango");
 console.log(frutas); // ["Manzana", "Banana", "Naranja", "Mango"]
+frutas.pop();
+console.log(frutas); // ["Manzana", "Banana", "Naranja"]
