3132700e6acb3546a79f12669b5e3631144bd662
Fernandomastre32 <20233l001062@utcv.edu.mx>
2024-10-04 18:19:55 -0600

    feat: Se agrega como comentario la forma de recorrer el arreglo tipo map()

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ for (let i = 0; i < frutas.length; i++) {
     console.log(frutas[i]);
   }
   
+
+  //const numeros = [1, 2, 3, 4];
+  //const dobles = numeros.map(num => num * 2);
+  //console.log(dobles); // [2, 4, 6, 8]
