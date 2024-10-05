b568a233920b942ab9e08f4fe47b9d304f6c88a2
Fernandomastre32 <20233l001062@utcv.edu.mx>
2024-10-04 18:14:44 -0600

    feat: Se Crean mas arreglos

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const frutas = ["Manzana", "Banana", "Naranja"];
 console.log(frutas[0]); // "Manzana"
 console.log(frutas[1]); // "Banana"
+frutas.push("Mango");
+console.log(frutas); // ["Manzana", "Banana", "Naranja", "Mango"]
